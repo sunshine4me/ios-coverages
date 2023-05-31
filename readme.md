4d034c1a430c095f3a31edacfba7115e6cb45e25
sunshine4me <176560744@qq.com>
2023-05-31 17:15:40 +0800

    添加 分析模块和 测试模块

---
+++ readme.md
@@ -0,0 +1,16 @@
+安装依赖
+
+```
+npm install
+```
+
+启动服务
+```
+node ./lib/index.js \
+--db.host 172.21.0.61 \
+--db.database ios-dev \
+--db.username root \
+--db.password 123456 \
+--redis.host 172.21.0.61 \
+--redis.password %20#m5yW7
+```
---
+++ test/test_iosAnalysisService.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const fs = require("fs");
+const { fileMerge, fileCov, tempFiles } = require("../lib/service/iosAnalysisService");
+
+const bufferData = fs.readFileSync(path.join(__dirname, "./file.profraw"));
+const objectFile = path.join(__dirname, "../objectFiles/test_objectfile");
+
+const fm = new fileMerge();
+fm.inputFile(bufferData);
+fm.merge().then((profile) => {
+  const fc = new fileCov(objectFile);
+  fc.cov(profile).then((e) => {
+    console.log(e.stdout);
+  });
+});
+
+setTimeout(() => {
+  console.log(tempFiles);
+}, 5000);
